c7e8fda633c616399ae1f4db815d9d5ea6cd6eaa
Dylan Long <dylan.long1115@gmail.com>
2018-10-26 03:18:07 -0500

    Clients Css done

--- src/Components/Pages/Clients/Clients.js
+++ src/Components/Pages/Clients/Clients.js
@@ -15,19 +15,23 @@ const clients = () => (
 		<Header/>
 		<div className="ClientPageWrap">
 			<h1><span>CLIENTS</span></h1>
-			<hr/>
-			<p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+			<div className="Line"/>
+			<p className="ClientPara">I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+			<div className="Line1">
 			<img src={Logo1} alt="Clients"/>
 			<img src={Logo2} alt="Clients"/>
 			<img src={Logo3} alt="Clients"/>
+			</div>
+			<div className="Line2">
 			<img src={Logo4} alt="Clients"/>
 			<img src={Logo5} alt="Clients"/>
 			<img src={Logo6} alt="Clients"/>
+			</div>
 			<div className="ClientsBox">
 				<p>“I'm a testimonial. Click me to edit and add text that says something nice about you and your services. Click me to edit and add text that says something nice about you and your services.”</p>
 			</div>
 			<div className="ArrowDiv"></div>
-			<p>Samantha Jones, Project Manager</p>
+			<p className="Sam">Samantha Jones, Project Manager</p>
 		</div>
 		<Footer/>
 	</div>
